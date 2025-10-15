81c138bc1af310891cf6643ba52b20433ff7c9d6
BIANCA CORTI SOUZA DE PAULA <bianca_c_paula@estudante.sesisenai.org.br>
2025-10-15 18:50:07 -0300

    Modifiquei a classe Produtos

--- Java 23/src/vendas/produtos/Produto.java
+++ Java 23/src/vendas/produtos/Produto.java
@@ -7,4 +7,57 @@ public class Produto {
     double preco;
     double custoMedio;
     int estoque;
+
+// ID
+public int getId(){
+    return this.id;
+}
+
+public void setId(int id){
+    this.id = id;
+}
+
+// Nome
+public String getNome(){
+    return this.nome;
+}
+
+public void setNome(String nome){
+    this.nome = nome;
+}
+
+// Codigo de Barras
+public String getCodigoBarras(){
+    return this.codigoBarras;
+}
+public void setCodigoBarras(String codigoBarras){
+    this.codigoBarras = codigoBarras;
+}
+
+// Preço
+public double getPrecos(){
+    return this.preco;
+}
+public void setPreco(Double  preco){
+    this.preco = preco;
+}
+
+// Custo Médio
+public double getCustoMedio(){
+    return this.custoMedio;
+}
+public void setCustoMedio(Double custoMedio){
+    this.custoMedio = custoMedio;
+}
+
+// Estoque
+public int getEstoque(){
+    return this.estoque;
+}
+
+public void setEstoque(int estoque){
+    this.estoque = estoque;
+}
+
+
 }
