6418feb0047dd7c7159acedb978f838b97332f59
Cadu-f12 <cadudailha68@gmail.com>
2025-10-16 12:14:05 -0300

    Classe cliente encapsulada

--- Java 23/src/vendas/clientes/Cliente.java
+++ Java 23/src/vendas/clientes/Cliente.java
@@ -1,8 +1,36 @@
 package vendas.clientes;
 
 public class Cliente {
-    int id;
-    String nome;
-    String telefone;
-    Categoria categoria;
+    private int id;
+    private String nome;
+    private String telefone;
+    private Categoria categoria;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
