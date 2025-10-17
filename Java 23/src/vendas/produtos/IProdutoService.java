cc335bee412310b63985c78f36fcaa0cdf9f8b18
Bianca <bianca_c_paula@estudante.sesisenai.org.br>
2025-10-17 13:12:25 -0300

    Modifiquei o ProdutoService

---
+++ Java 23/src/vendas/produtos/IProdutoService.java
@@ -0,0 +1,9 @@
+package vendas.produtos;
+
+import vendas.clientes.Cliente;
+
+public interface IProdutoService {
+    void cadastroCliente(Cliente cliente);
+    Cliente consultarCliente(int id);
+    Cliente[] listaCliente();
+}
--- Java 23/src/vendas/produtos/ProdutoService.java
+++ Java 23/src/vendas/produtos/ProdutoService.java
@@ -1,5 +1,33 @@
 package vendas.produtos;
 
-public class ProdutoService {
-    // AQUI ENTRA AS REGRAS DE NEGÓCIO!
+import vendas.clientes.Cliente;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProdutoService implements IProdutoService{
+    private Map<Integer, Cliente> clientes;
+
+    public void ClienteService (){
+        this.clientes = new HashMap<>();
+    }
+    public ProdutoService(Map<Integer, Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+
+    @Override
+    public void cadastroCliente(Cliente cliente) {
+
+    }
+
+    @Override
+    public Cliente consultarCliente(int id) {
+        return null;
+    }
+
+    @Override
+    public Cliente[] listaCliente() {
+        return new Cliente[0];
+    }
 }
